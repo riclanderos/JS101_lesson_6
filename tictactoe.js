81fdad9101293aec2bc4e89c2eb062f7346d71c3
Ricardo Landeros <landerosr81@gmail.com>
2022-08-29 22:11:18 -0700

    tictactoe.js

---
+++ tictactoe.js
@@ -0,0 +1,28 @@
+function displayBoard(board) {
+  console.log('');
+  console.log('     |     |');
+  console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
+  console.log('     |     |');
+  console.log('-----+-----+-----');
+  console.log('     |     |');
+  console.log(`  ${board['4']}  |  ${board['5']}  |  ${board['6']}`);
+  console.log('     |     |');
+  console.log('-----+-----+-----');
+  console.log('     |     |');
+  console.log(`  ${board['7']}  |  ${board['8']}  |  ${board['9']}`);
+  console.log('     |     |');
+  console.log('');
+}
+
+function initializeBoard() {
+  let board = {};
+
+  for (let square = 1; square <= 9; square++) {
+    board[String(square)] = ' ';
+  }
+
+  return board;
+}
+
+let board = initializeBoard();
+displayBoard(board);
